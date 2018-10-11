e9f963bb4ef513390d4d9bde70fd4bafaf73512f
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-10-11 10:50:27 +0200

    Add files via upload

---
+++ Method header exercise/KlasseSammenProgrammering.java
@@ -0,0 +1,48 @@
+public class KlasseSammenProgrammering {
+    //Metode headers oevelse
+    //
+    public static void increaseHeat()   { // Returnerer temperatur
+
+        /* som læser den nuværende temperatur ud i Celsius
+         som læser den nuværende temperatur ud i Kelvin
+        som returnerer de sidste 5 temperaturer,
+        som returnerer de sidste x temperaturer
+           som returnerer
+        som genstarter måleren
+        som sender en Opdateringsobjekt til måleren
+        som modtager et kanalnummer (int) for kommunikation
+            constructoren skal tage imod smartsensorens IP-nummer eller serienummer (int), eller begge dele.**/
+
+
+        // 1)
+        public static void temperature(int a, char b)   {    // set a to value 1 and char = C
+        }
+
+        // 2)
+        public static void temperature(int a, char b)   {   // set a to value 1 and char = K
+
+
+    }
+        // 3)
+        public static void temperature(int a, char b)  // set a to value 5 and char to C or K
+
+        // 4)
+        public static void temperature(int a, char b)  // set a to value x and char to C or K
+
+        // 5)
+        public static void tempReturn(boolean)    {
+
+        // 6)
+        public static boolean tempReset(boolean)
+
+        // 7 )
+        public static void temupdateobj()
+                fetch file/object from import location
+
+        // 8)
+        public static int channelHz(int CHz)
+
+        // 9)
+        reciveNumber(int SIP, int SE,)
+
+        }
